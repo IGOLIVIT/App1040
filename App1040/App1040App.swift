9aaec77abf28cc0769451fb21ff6812df6340a6a
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-11-06 19:59:47 +0300

    Initial Commit

---
+++ App1040/App1040App.swift
@@ -0,0 +1,17 @@
+//
+//  App1040App.swift
+//  App1040
+//
+//  Created by IGOR on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct App1040App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App1040/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App1040
+//
+//  Created by IGOR on 06/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
